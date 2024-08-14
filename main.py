2a8d0436b968875243db5d48c49ce3938ab7cb53
Krish Veera <krishv@usf.edu>
2024-08-14 01:35:50 -0400

    catalog classes

---
+++ main.py
@@ -0,0 +1,208 @@
+catalog_2021_semester_breakdown = {
+    "semester_1": {
+        "Natural Science Elective": False,
+        "Calculus I": False,
+        "Foundations of Engineering Lab": False,
+        "Foundations of Engineering": False,
+        "Composition I": False,
+    },
+    "semester_2": {
+        "Physics I": False,
+        "Physics I Lab": False,
+        "Calculus II": False,
+        "Programming Concepts": False,
+        "Composition II": False,
+    },
+    "semester_3": {
+        "Physics II": False,
+        "Physics II Lab": False,
+        "Calculus III": False,
+        "Computer Organization": False,
+        "Program Design": False,
+    },
+    "semester_4": {
+        "Computer Logic Design Lab": False,
+        "Computer Logic Design": False,
+        "Data Structures": False,
+        "Discrete Structures": False,
+        "Computer Logic Design Lab": False,
+    },
+    "semester_5": {
+        "Probability and Statistics for Engineers": False,
+        "Natural Science Elective": False,
+        "Social Science Elective": False,
+    },
+    "semester_6": {
+        "Linear Systems": False,
+        "CSE Elective": False,
+        "Computer Architecture": False,
+        "Computer Architecture Lab": False,
+        "CSE Software Elective": False,
+        "Human and Cultural Diversity Elective": False,
+    },
+    "semester_7": {
+        "CSE Elective": False,
+        "CSE Software Elective": False,
+        "Analysis of Algorithms": False,
+        "Communication for Engineers": False,
+        "General Elective": False,
+    },
+    "semester_8": {
+        "CSE Elective": False,
+        "CSE Theory Elective": False,
+        "Secure Coding": False,
+        "Operating Systems": False,
+        "General Elective": False,
+    },
+    "semester_9": {
+        "CSE Elective": False,
+        "Software Engineering": False,
+        "Ethical Issues and Professional Conduct": False,
+        "General Elective": False,
+    },
+}
+
+catalog_2021_class_type_breakdown = {
+    "general_education": {
+        "Calculus I": False,
+        "Foundations of Engineering Lab": False,
+        "Foundations of Engineering": False,
+        "Composition I": False,
+        "Physics I": False,
+        "Physics I Lab": False,
+        "Calculus II": False,
+        "Physics II": False,
+        "Physics II Lab": False,
+        "Composition II": False,
+        "Calculus III": False,
+    },
+    "major_requirements": {
+        "Programming Concepts": False,
+        "Computer Organization": False,
+        "Program Design": False,
+        "Computer Logic Design Lab": False,
+        "Computer Logic Design": False,
+        "Data Structures": False,
+        "Discrete Structures": False,
+        "Computer Logic Design Lab": False,
+        "Probability and Statistics for Engineers": False,
+        "Linear Systems": False,
+        "Computer Architecture": False,
+        "Computer Architecture Lab": False,
+        "Analysis of Algorithms": False,
+        "Communication for Engineers": False,
+        "Secure Coding": False,
+        "Operating Systems": False,
+        "Software Engineering": False,
+        "Ethical Issues and Professional Conduct": False,
+    },
+    "electives": {
+        "Natural Science Elective I": False,
+        "Social Science Elective": False,
+        "Human and Cultural Diversity Elective": False,
+        "Natural Science Elective II": False,
+        "CSE Software Elective I": False,
+        "CSE Software Elective II": False,
+        "CSE Elective I": False,
+        "CSE Elective II": False,
+        "CSE Elective III": False,
+        "CSE Elective IV": False,
+        "CSE Theory Elective": False,
+        "General Elective I": False,
+        "General Elective II": False,
+        "General Elective III": False,
+    },
+}
+
+catalog_2021_all_classes_breakdown = {
+    "Calculus I": False,  # MAC2281, MAC2311
+    "Foundations of Engineering Lab": False,  # EGN3000L
+    "Foundations of Engineering": False,  # EGN3000
+    "Composition I": False,  # ENC1101
+    "Physics I": False,  # PHY2048
+    "Physics I Lab": False,  # PHY2048L
+    "Calculus II": False,  # MAC2282, MAC2312
+    "Physics II": False,  # PHY2049
+    "Physics II Lab": False,  # PHY2049L
+    "Composition II": False,  # ENC1102
+    "Calculus III": False,  # MAC2283, MAC2313
+    "Programming Concepts": False,  # COP2510
+    "Computer Organization": False,  # CDA3103
+    "Program Design": False,  # COP3514
+    "Computer Logic Design Lab": False,  # CDA3201L
+    "Computer Logic Design": False,  # CDA3201
+    "Data Structures": False,  # COP4530
+    "Discrete Structures": False,  # COT3100
+    "Probability and Statistics for Engineers": False,  # EGN3443
+    "Linear Systems": False,  # ENG4450
+    "Computer Architecture": False,  # CDA4205
+    "Computer Architecture Lab": False,  # CDA4205L
+    "Analysis of Algorithms": False,  # COT4400
+    "Communication for Engineers": False,  # ENC3246
+    "Secure Coding": False,  # CNT4419
+    "Operating Systems": False,  # COP4600
+    "Software Engineering": False,  # CEN4020
+    "Ethical Issues and Professional Conduct": False,  # CIS4250
+    "Natural Science Elective I": False,  # NSE1
+    "Social Science Elective": False,  # SSE1
+    "Human and Cultural Diversity Elective": False,  # HCDE1
+    "Natural Science Elective II": False,  # NSE2
+    "CSE Elective": False,  # CSE1
+    "CSE Software Elective I": False,  # CSE2
+    "CSE Software Elective II": False,  # CSE3
+    "CSE Elective I": False,  # CSE4
+    "CSE Elective II": False,  # CSE5
+    "CSE Elective III": False,  # CSE6
+    "CSE Theory Elective": False,  # CSE7
+    "General Elective I": False,  # GE1
+    "General Elective II": False,  # GE2
+    "General Elective III": False,  # GE3
+}
+
+catalog_2021_all_classes_codes_breakdown = {
+    "MAC2281": False,  # Calculus I - viable to get credit for AP Calc AB (score of 3 or higher)
+    "MAC2311": False,  # Calculus I - viable to get credit for AP Calc AB (score of 3 or higher)
+    "MAC2282": False,  # Calculus II - viable to get credit for AP Calc BC (score of 4 or higher)
+    "MAC2312": False,  # Calculus II - viable to get credit for AP Calc BC (score of 4 or higher)
+    "MAC2283": False,  # Calculus III
+    "MAC2313": False,  # Calculus III
+    "EGN3000L": False,  # Foundations of Engineering Lab
+    "EGN3000": False,  # Foundations of Engineering
+    "ENC1101": False,  # Composition I - viable to get credit for AP English Lang or AP English Lit (score of 3 or higher)
+    "ENC1102": False,  # Composition II - viable to get credit for AP English Lang or AP English Lit (score of 4 or higher)
+    "PHY2048": False,  # Physics I - viable to get credit for AP Physics C: Mech (score of 4 or higher)
+    "PHY2048L": False,  # Physics I Lab - viable to get credit for AP Physics C: Mech (score of 4 or higher)
+    "PHY2049": False,  # Physics II - viable to get credit for AP Physics C: EnM (score of 4 or higher)
+    "PHY2049L": False,  # Physics II Lab - viable to get credit for AP Physics C: EnM (score of 4 or higher)
+    "COP2510": False,  # Programming Concepts
+    "CDA3103": False,  # Computer Organization
+    "COP3514": False,  # Program Design
+    "CDA3201L": False,  # Computer Logic Design Lab
+    "CDA3201": False,  # Computer Logic Design
+    "COP4530": False,  # Data Structures
+    "COT3100": False,  # Discrete Structures
+    "EGN3443": False,  # Probability and Statistics for Engineers
+    "ENG4450": False,  # Linear Systems
+    "CDA4205": False,  # Computer Architecture
+    "CDA4205L": False,  # Computer Architecture Lab
+    "COT4400": False,  # Analysis of Algorithms
+    "ENC3246": False,  # Communication for Engineers
+    "CNT4419": False,  # Secure Coding
+    "COP4600": False,  # Operating Systems
+    "CEN4020": False,  # Software Engineering
+    "CIS4250": False,  # Ethical Issues and Professional Conduct
+    "NSE1": False,  # Natural Science Elective I
+    "SSE1": False,  # Social Science Elective 
+    "HCDE1": False,  # Human and Cultural Diversity Elective
+    "NSE2": False,  # Natural Science Elective II
+    "CSE1": False,  # CSE Elective
+    "CSE2": False,  # CSE Software Elective I
+    "CSE3": False,  # CSE Software Elective II
+    "CSE4": False,  # CSE Elective I
+    "CSE5": False,  # CSE Elective II
+    "CSE6": False,  # CSE Elective III
+    "CSE7": False,  # CSE Theory Elective
+    "GE1": False,  # General Elective I
+    "GE2": False,  # General Elective II
+    "GE3": False,  # General Elective III
+}
